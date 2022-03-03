a699374cd85de2c32b87ae5dbdef90bd09c76559
Ticiane Silva <test.ticianesilva@gmail.com>
2022-03-03 20:26:10 -0300

    Calculadora

---
+++ calculadora/main.py
@@ -0,0 +1,34 @@
+# Criar uma calculador
+# O Usuário deve escolher uma das operações
+# O Usuário deve inserir 2 valores
+# O Programa exibe o resultado de acordo com a operação escolida e os valores diferentes
+
+
+numero1 = int(input("Digite o primeiro valor: "))
+operacao = input("Digite o operador")
+numero2 = int(input("Digite o segundo valor:"))
+
+if operacao == "+":
+  resultado = numero1 + numero2
+
+elif operacao == "-":
+  resultado = numero1 - numero2
+
+elif operacao == "*":
+  resultado = numero1 * numero2
+
+elif operacao == "/":
+  resultado = numero1 / numero2
+
+else:
+  resultado = "Operação inválida"
+
+print(str(numero1) + "" + str(operacao) + "" + str(numero2) + "=" + str (resultado))
+
+
+
+
+
+
+
+
